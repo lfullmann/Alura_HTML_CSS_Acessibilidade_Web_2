18a42a18fce49004eb4f110424fcb8b503955b39
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-05-18 20:26:59 -0300

    Refactory

--- js/carousel.js
+++ js/carousel.js
@@ -1,44 +1,48 @@
-//Variaveis
+// Variaveis
 
-var btns = document.querySelectorAll(".listaDeArtigos-slider-item");
-var new0 = document.querySelector("#new0");
-var new1 = document.querySelector("#new1");
-var new2 = document.querySelector("#new2");
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var noticias = document.querySelectorAll('.listaDeArtigos-item');
 
-new0.style.display = block;
 
-//Criando indicador de slide atual
-var indicadorSlideAtual = document.createElement("span");
-indicadorSlideAtual.classList.add("escondeVisualmente");
-indicadorSlideAtual.id = "escondeVisualmente";
-indicadorSlideAtual.textContent = "(Slide Atual)";
+var new0 = document.querySelector('#new0');
+var new1 = document.querySelector('#new1');
+var new2 = document.querySelector('#new2');
+
+
+new0.style.display = 'block'
+
+
+// Criando indicador de slide atual
+var indicadorSlideAtual = document.createElement('span');
+indicadorSlideAtual.classList.add('escondeVisualmente');
+indicadorSlideAtual.textContent = '(Slide atual)';
+
 
 // Percorre todos os botoes controladores
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    if (this.getAttribute("data-sliderItem") === "0") {
-      new0.style.display = "block";
-      new1.style.display = "none";
-      new2.style.display = "none";
-    } else if (this.getAttribute("data-sliderItem") === "1") {
-      new0.style.display = "none";
-      new1.style.display = "block";
-      new2.style.display = "none";
-    } else {
-      new0.style.display = "none";
-      new1.style.display = "none";
-      new2.style.display = "block";
-    }
-    document
-      .querySelector(".listaDeArtigos-slider-item .escondeViscualmente")
-      .remove();
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    
+    // btn.href = 'javascript:void(0)'
+    noticias.forEach(function(noticia) {
+      noticia.style.display = 'none';
+      
+      if ( this.getAttribute('data-sliderItem') === noticia.getAttribute('data-noticia') ) {
+        noticia.style.display = 'block'
+      }
+
+    }.bind(this));
+    
+  
+    
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
     this.append(indicadorSlideAtual);
+    
 
     // Remove classe 'ativo' dos outros botoes
-    btns.forEach(function (btnRemoveClass) {
-      btnRemoveClass.classList.remove("listaDeArtigos-slider-item--ativo");
-    });
+    btns.forEach(function(btnRemoveClass) {
+      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
+    })
 
-    this.classList.add("listaDeArtigos-slider-item--ativo");
-  });
-});
+    this.classList.add('listaDeArtigos-slider-item--ativo')
+  })
+})
--- js/dialog.js
+++ js/dialog.js
@@ -1,37 +1,46 @@
 // Variáveis
-var conteudoForaDialog = document.querySelector("#conteudoForaDialog");
-var btnAbreDialog = document.querySelector("#abreDialog");
-var dialog = document.querySelector(".dialogNewsletter");
-var dialogBody = document.querySelector(".dialogNewsletter-body");
-var dialogOverlay = document.querySelector(".dialogNewsletter-overlay");
-var video = document.querySelector("video");
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
 
-btnAbreDialog.style.display = "block";
+var btnAbreDialog = document.querySelector('#abreDialog');
+var dialog = document.querySelector('.dialogNewsletter');
+var dialogBody = document.querySelector('.dialogNewsletter-body');
+var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
-//Quando abrir a dialog...
-btnAbreDialog.addEventListener("click", function () {
-  dialog.classList.add("dialogNewsletter--aberto");
-  document.querySelector(".dialogNewsletter-label").focus();
-  conteudoForaDialog.inert = "true";
-  video.removeAttribute("controls");
+btnAbreDialog.style.display = 'block'
+
+// Quando abrir a dialog...
+btnAbreDialog.addEventListener('click', function() {
+  dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-label').focus();
+  conteudoForaDialog.inert = true;
+
+  document.querySelector('video').removeAttribute('controls'); 
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove("dialogNewsletter--aberto");
+  dialog.classList.remove('dialogNewsletter--aberto'); 
   conteudoForaDialog.inert = false;
   btnAbreDialog.focus();
-  video.setAttribute("controls", true);
+  document.querySelector('video').setAttribute('controls', 'true');
 }
 
 // Listeners
-document
-  .querySelector(".dialogNewsletter-fechar")
-  .addEventListener("click", fechandoDialog);
-dialogOverlay.addEventListener("click", fechandoDialog);
+document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener('click', fechandoDialog);
 
-document.addEventListener("keyup", function (evento) {
+document.addEventListener('keyup', function(evento) {
   if (evento.keyCode == 27) {
-    fechandoDialog();
+      fechandoDialog()
   }
-});
+})
+
+
+
+
+
+
+
+
+

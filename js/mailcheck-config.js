46820f21526735231a78d42c243282910e15ef1d
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-05-18 22:09:42 -0300

    Refactory

--- js/mailcheck-config.js
+++ js/mailcheck-config.js
@@ -1,23 +1,24 @@
-var campoEmail = document.querySelector("#email");
-var sugestao = document.querySelector("#sugestao");
+var campoEmail = document.querySelector('#email')
+var sugestao = document.querySelector('#sugestao')
 
-var domains = ["gmail.com", "aol.com", "outlook.com"];
-var secondLevelDomains = ["hotmail"];
-var topLevelDomains = ["com", "net", "org", "br"];
+var domains = ['gmail.com', 'aol.com'];
+var secondLevelDomains = ['hotmail']
+var topLevelDomains = ["com", "net", "org"];
 
-campoEmail.addEventListener("blur", function () {
+
+campoEmail.addEventListener('blur', function() {
   Mailcheck.run({
     email: campoEmail.value,
-    domains: domains, // optional
-    topLevelDomains: topLevelDomains, // optional
+    domains: domains,                       // optional
+    topLevelDomains: topLevelDomains,       // optional
     secondLevelDomains: secondLevelDomains, // optional
-    suggested: function (suggestion) {
-      sugestao.style.display = "inline-block ";
-      sugestao.textContent = "Você quis dizer:" + suggestion.full + "?";
-      sugestao.parentNode.classList.add("contatoCampo--erro");
-      campoEmail.classList.add("contatoCampo--validouErro");
-      sugestao.setAttribute("tabindex", "0");
-      sugestao.setAttribute("role", "alert");
-    },
+    suggested: function(suggestion) {
+      sugestao.style.display = 'inline-block';
+      sugestao.textContent = 'Você quis dizer: ' + suggestion.full + ' ?';
+      sugestao.parentNode.classList.add('contatoCampo--erro');
+      campoEmail.classList.add('contatoCampo--validouErro');
+      sugestao.setAttribute('tabindex', '0');
+      sugestao.setAttribute('role', 'alert');
+    }
   });
 });
--- js/validacao-cep.js
+++ js/validacao-cep.js
@@ -1,10 +1,8 @@
-var campoCep = document.querySelector("#cep");
-campoCep.oninvalid = function () {
-  this.setCustomValidity("");
-
-  if (this.validity.valid) {
-    this.setCustomValidity("Ops! Tem algo errado neste campo!");
-
-    this.parentNode.classList.add("contatoCampo--erro");
-  }
-};
+document.querySelector('#cep').oninvalid = function() {
+  this.setCustomValidity('');
+  
+  if (!this.validity.valid) {
+      this.setCustomValidity('Ops! Tem algo errado neste campo!');
+      this.parentNode.classList.add('contatoCampo--erro')
+   }
+}
